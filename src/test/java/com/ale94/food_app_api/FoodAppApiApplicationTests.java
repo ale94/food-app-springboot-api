c477c7a9a3515419cccb897eb17eb23274ae4109
ale94 <alejandrorua111@gmail.com>
2025-10-29 23:59:34 -0300

    init: initial commit

---
+++ src/test/java/com/ale94/food_app_api/FoodAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ale94.food_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
